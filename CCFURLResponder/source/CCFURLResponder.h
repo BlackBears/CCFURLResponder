70470c7517e5e6a7b5d3508f4a5e4189aba170ba
Alan K. Duncan <alan@cocoa-factory.com>
2013-09-30 06:15:02 -0500

    documentation

--- CCFURLResponder/source/CCFURLResponder.h
+++ CCFURLResponder/source/CCFURLResponder.h
@@ -7,11 +7,52 @@
  *   @note Copyright 2013 Cocoa Factory, LLC.  All rights reserved
  */
 
+/**
+ *	CCFURLResponder encapsulates the methods that allow applications to declare patterns of URLs to which they respond
+    and generate \c NSNotification objects that can be posted in a notification center, allowing classes in the 
+    application to respond to received URL events.
+ */
 @interface CCFURLResponder : NSObject
 
+/**
+ *	Register a scheme
+ *
+ *	@param	pattern	The pattern string for the scheme.  Patterns adhere to a simple but strict format langage.  Path components are designated by single '/' characters.  Within 
+ *                  each component, there must be a single type character from the set \b s, \b l, \b i, \b f for string, literal, integer, and float, respectively.  For string, integer,
+                    and float components, the component patterns will look like: \c /s:YourStringKeyName/i:YourIntegerKeyName/f:YourFloatKeyName.  The keys become dictionary keys in the \c NSNotification
+ *                  userInfo dictionary.  In the case of literals, the pattern syntax is slightly different:  \c /l:(animal)AnimalPathKey
+ *	@param	notification	The notification name for this pattern.
+ *	@param	error	An optional error object.
+ *
+ *	@return	Returns \c YES if the registration succeeds, otherwise \c NO.
+ */
 + (BOOL)registerSchemeWithPattern:(NSString *)pattern forNotificationName:(NSString *)notification error:(NSError *__autoreleasing *)error;
+
+/**
+ *	Process an incoming URL
+ *
+ *  Looks for a scheme pattern that matches the URL.  If a pattern is found then create a notification for the pattern and 
+ *  post the notification to the default notification center.
+ *
+ *	@param	url	The NSURL object to process
+ *
+ *	@return	Returns \c YES if the URL could be processed, otherwise \c NO.
+ */
 + (BOOL)processURL:(NSURL *)url;
+
+/**
+ *	Searches for a pattern matching the provided URL.  If a match is found, creates an \c NSNotification object according
+ *  to the pattern specifications.
+ *
+ *	@param	url	The URL to match.
+ *
+ *	@return	Returns an \c NSNotification object if the pattern matches a known scheme.  Otherwise, returns \c nil.
+ */
 + (NSNotification *)notificationWithURL:(NSURL *)url;
+
+/**
+ *	Removes all registered schemes.
+ */
 + (void)resetSchemes;
 
 @end
--- CCFURLResponder/source/CCFURLResponderScheme.h
+++ CCFURLResponder/source/CCFURLResponderScheme.h
@@ -7,14 +7,50 @@
  *   @note Copyright 2013 Cocoa Factory, LLC.  All rights reserved
  */
 
+/**
+ *	CCFURLResponderScheme instances encapsulate the properties and methods of a class of objects that represent an incoming URL
+ *  to which the application can respond.
+ */
 @interface CCFURLResponderScheme : NSObject
 
+/**
+ *	The name of the notification generated when the pattern matches a URL
+ */
 @property (nonatomic, copy) NSString *notificationName;
+
+/**
+ *	An array of userInfo keys that are attached to the \c NSNotification object
+ */
 @property (nonatomic, copy) NSArray *keys;
+
+/**
+ *	An array of pattern component types
+ */
 @property (nonatomic, copy) NSArray *types;
+
+/**
+ *	An array of pattern component literals, if applicable.
+ */
 @property (nonatomic, copy) NSArray *literals;
 
+/**
+ *	Sets the pattern.
+ *
+ *
+ *	@param	pattern	The string pattern from which to create the scheme
+ *	@param	error	An option \c NSError object
+ *
+ *	@return	Returns \c YES if the pattern could be parsed, otherwise \c NO
+ */
 - (BOOL)setPattern:(NSString *)pattern error:(NSError *__autoreleasing *)error;
+
+/**
+ *	Creates a notification from the URL provided
+ *
+ *	@param	url	The URL to parse for a match
+ *
+ *	@return	Returns the notification if the pattern matches, otherwise \c nil.
+ */
 - (NSNotification *)notificationWithURL:(NSURL *)url;
 
 @end
